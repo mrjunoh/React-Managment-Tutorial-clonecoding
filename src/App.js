cac7d0a35c92ba2c93cc66eb29f63c291331dc16
mrjunoh <mrjunoh@naver.com>
2023-01-17 17:35:51 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,101 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import React, {Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers=[{
+  'id':1,
+  'image':'https://placeimg.com/64/64/any',// palceimg 웹 사이트는 랜덤이미지를 보여주는 사이트 64*64크기로 보여달라는 뜻
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'man',
+  'job':'college student'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'철수',
+  'birthday':'13535',
+  'gender':'man',
+  'job':'초등학생'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'영희',
+  'birthday':'32567',
+  'gender':'여자',
+  'job':'직장인'
+},
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="log"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c=>{
+          //c는 그냥 int i같은 변수명인듯?
+          return (<Customer
+            key={c.id} // map을 사용하면 key를 사용해 각 원소를 구별가능한 key를 설정해야한다. 무조건
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+          );
+        })
+      }
     </div>
+
+    //customer는 우리가 만든 class로 customer의 내용이 출력됨
+    //위 예시 name~job까지가 여기서 바로 내용을 추가도가능하다. 이걸 props 프롭스라고 부름
+    
+    //고객이 많아져서 반복문 for문처럼 사용가능한게 "map"이다.
+    //map안에도 return()안에 내용 넣기
+
   );
 }
 
 export default App;
+
+
+
+
+
+
+// 이부분은 map사용하기 전 코드
+// function App() {
+//   return (
+//     <div>
+//       <Customer
+//         id={customers[0].id}
+//         image={customers[0].image}
+//         name={customers[0].name}
+//         birthday= {customers[0].birthday}
+//         gender={customers[0].gender}
+//         job={customers[0].job}
+//       />
+//       <Customer
+//         id={customers[1].id}
+//         image={customers[1].image}
+//         name={customers[1].name}
+//         birthday= {customers[1].birthday}
+//         gender={customers[1].gender}
+//         job={customers[1].job}
+//       />
+//       <Customer
+//         id={customers[2].id}
+//         image={customers[2].image}
+//         name={customers[2].name}
+//         birthday= {customers[2].birthday}
+//         gender={customers[2].gender}
+//         job={customers[2].job}
+//       />      
+//     </div>
+
+//     //customer는 우리가 만든 class로 customer의 내용이 출력됨
+//     //위 예시 name~job까지가 여기서 바로 내용을 추가도가능하다. 이걸 props 프롭스라고 부름
+//   );
+// }
---
+++ src/components/Customer.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+//React.Component는 일종의 라이브러리이자 class임
+//즉 React.Component를 상속받아서 class를 새롭게 만들었음
+class Customer extends React.Component{
+    //React.Component여기에 render()가 포함되어있어 
+    //화면에 나올 내용이 render()안에 있다.
+    //render()함수는 출력내용이 return()안에 있어야함
+    render(){
+        return(
+            <div>
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}></CustomerProfile>
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}></CustomerInfo>
+            </div>
+        )
+    }
+
+}
+
+//고객의 정보가 많으면 component를 분리해서 관리하면됨
+class CustomerProfile extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;//내보내기 코드
